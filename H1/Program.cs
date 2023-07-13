76a6b1d7f9f5156634bc7e2effe8dddb8b8db0f2
Anton Gusev <antongusev705@gmail.com>
2023-07-13 13:35:04 +0300

    H1

---
+++ H1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+ Console.Write("Введите пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Проверяем, является ли число палиндромом
+        bool isPalindrome = IsPalindrome(number);
+
+        // Выводим результат
+        if (isPalindrome)
+            Console.WriteLine("Число является палиндромом.");
+        else
+            Console.WriteLine("Число не является палиндромом.");
+
+        Console.ReadLine();
+    
+
+    static bool IsPalindrome(int number)
+    {
+        // Преобразуем число в строку
+        string str = number.ToString();
+
+        // Проверяем, является ли строка палиндромом
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+
